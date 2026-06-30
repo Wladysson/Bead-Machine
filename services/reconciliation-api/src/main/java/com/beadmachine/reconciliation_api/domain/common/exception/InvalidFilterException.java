eedec30430d38e6ecb57532ea7481678be3d6c2e
wladysson <wladysonaraujo991@gmail.com>
2026-06-30 08:37:29 -0300

    feat(domain-common): introduce invalid filter exception for search validation failures

---
+++ services/reconciliation-api/src/main/java/com/beadmachine/reconciliation_api/domain/common/exception/InvalidFilterException.java
@@ -0,0 +1,9 @@
+package com.beadmachine.reconciliation.api.domain.common.exception;
+
+public class InvalidFilterException extends DomainException {
+
+    public InvalidFilterException(String message) {
+        super(message);
+    }
+
+}
